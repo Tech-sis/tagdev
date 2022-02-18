b20593ad8e2b558b193a5c5e19eb7052921b4c25
Adeola Abilawon <abilawonadeola@gmail.com>
2022-02-18 17:10:31 +0100

    authenticate users

---
+++ src/components/authentication/vendor/RegisterVendor.js
@@ -0,0 +1,174 @@
+/* eslint-disable prettier/prettier */
+import * as Yup from 'yup';
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
+import { useFormik, Form, FormikProvider } from 'formik';
+import eyeFill from '@iconify/icons-eva/eye-fill';
+import eyeOffFill from '@iconify/icons-eva/eye-off-fill';
+import { useNavigate } from 'react-router-dom';
+// material
+import { Stack, TextField, IconButton, InputAdornment } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+
+// logic
+import { collection, addDoc } from 'firebase/firestore';
+import { signUp, db, auth } from '../../../firebase';
+
+// ----------------------------------------------------------------------
+
+export default function RegisterForm() {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState('');
+
+  const RegisterSchema = Yup.object().shape({
+    firstName: Yup.string()
+      .min(2, 'Too Short!')
+      .max(50, 'Too Long!')
+      .required('First name required'),
+    lastName: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!').required('Last name required'),
+    phoneNumber: Yup.string()
+      .min(10, 'Too Short!')
+      .max(15, 'Too Long!')
+      .required('Phone Number is required'),
+    email: Yup.string().email('Email must be a valid email address').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+    companyName: Yup.string().required('Company Name is required'),
+    companyAddress: Yup.string().required('Company Address is required')
+  });
+
+  // eslint-disable-next-line no-undef
+
+  const userRef = collection(db, 'users');
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      phoneNumber: '',
+      email: '',
+      password: '',
+      companyName: '',
+      companyAddress: ''
+    },
+    validationSchema: RegisterSchema,
+    onSubmit: async (values) => {
+      console.log(values);
+
+      try {
+        await signUp(values.email, values.password);
+        const user = auth.currentUser;
+        await addDoc(userRef, {
+          uid: user.uid,
+          email: user.email,
+          displayName: `${values.firstName} ${values.lastName}`,
+          photoURL: user.photoURL,
+          companyName: values.companyName,
+          companyAddress: values.companyAddress,
+          role: 'vendor',
+          createdAt: new Date()
+        });
+        navigate('/dashboard');
+      } catch (err) {
+        setError(err.message);
+      }
+    }
+  });
+
+  const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik;
+
+  return (
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+            <TextField
+              fullWidth
+              label="First name"
+              {...getFieldProps('firstName')}
+              error={Boolean(touched.firstName && errors.firstName)}
+              helperText={touched.firstName && errors.firstName}
+            />
+
+            <TextField
+              fullWidth
+              label="Last name"
+              {...getFieldProps('lastName')}
+              error={Boolean(touched.lastName && errors.lastName)}
+              helperText={touched.lastName && errors.lastName}
+            />
+          </Stack>
+
+          <TextField
+            fullWidth
+            autoComplete="phoneNumber"
+            type="tel"
+            label="Phone Number"
+            {...getFieldProps('phoneNumber')}
+            error={Boolean(touched.phoneNumber && errors.phoneNumber)}
+            helperText={touched.phoneNumber && errors.phoneNumber}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="username"
+            type="email"
+            label="Email address"
+            {...getFieldProps('email')}
+            error={Boolean(touched.email && errors.email)}
+            helperText={touched.email && errors.email}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="current-password"
+            type={showPassword ? 'text' : 'password'}
+            label="Password"
+            {...getFieldProps('password')}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton edge="end" onClick={() => setShowPassword((prev) => !prev)}>
+                    <Icon icon={showPassword ? eyeFill : eyeOffFill} />
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="companyName"
+            type="text"
+            label="Company Name"
+            {...getFieldProps('companyName')}
+            error={Boolean(touched.companyName && errors.companyName)}
+            helperText={touched.companyName && errors.companyName}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="companyAddress"
+            type="text"
+            label="Company Address"
+            {...getFieldProps('companyAddress')}
+            error={Boolean(touched.companyAddress && errors.companyAddress)}
+            helperText={touched.companyAddress && errors.companyAddress}
+          />
+
+          <LoadingButton
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            loading={isSubmitting}
+          >
+            Register
+          </LoadingButton>
+        </Stack>
+      </Form>
+    </FormikProvider>
+  );
+}
